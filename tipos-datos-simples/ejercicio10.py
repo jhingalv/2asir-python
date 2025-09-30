1915d02dc1932d1890ed8135008fbc2499bd6bcc
Amador Hinojosa Gálvez (IZV) <you@example.com>
2025-09-30 14:17:52 +0200

    ADD ejercicio10.py

---
+++ tipos-datos-simples/ejercicio10.py
@@ -0,0 +1,8 @@
+#Ejercicio 10
+
+payasos = int(input("Número de payasos vendidos: "))
+muñecas = int(input("Número de muñecas vendidas: "))
+
+peso_total = (payasos * 112) + (muñecas * 75)
+
+print(f"El peso total del paquete es: {peso_total} gramos")
