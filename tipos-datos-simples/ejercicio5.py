11e6c364175710fe90ce84e08f4277078aea78f7
Amador Hinojosa Gálvez (IZV) <you@example.com>
2025-09-30 14:02:43 +0200

    ADD ejercicio5.py

---
+++ tipos-datos-simples/ejercicio5.py
@@ -0,0 +1,6 @@
+#Ejercicio 5
+
+horas_trabajadas = float(input("Introduce el número de horas trabajadas: "))
+coste_hora = float(input("Introduce el coste por hora: "))
+paga = horas_trabajadas * coste_hora
+print(f"La paga que te corresponde es: {paga:.2f}")
