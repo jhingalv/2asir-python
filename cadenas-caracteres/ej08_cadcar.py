c66d136728ba0e9822b47e2a9d5d6707ebc0e820
Amador Hinojosa Gálvez (IZV) <you@example.com>
2025-10-07 14:13:16 +0200

    ADD ejercicio 8 cadenas caracteres

---
+++ cadenas-caracteres/ej08_cadcar.py
@@ -0,0 +1,4 @@
+x8precio = input("Introduce el precio del producto (en euros, con dos decimales): ")
+x8euros, x8centimos = x8precio.split(".")
+print("Euros:", x8euros)
+print("Céntimos:", x8centimos)
