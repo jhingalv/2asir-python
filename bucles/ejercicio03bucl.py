fe99fcd81cfd10fc5b281be9c7a3e503be7f8b9b
Amador Hinojosa Gálvez (IZV) <you@example.com>
2025-11-04 13:59:39 +0100

    UPDATE ejercicio03bucl.py

--- bucles/ejercicio03bucl.py
+++ bucles/ejercicio03bucl.py
@@ -1,7 +1,7 @@
-x3int = int(input("Introduce tu un numero entero positivo: "))
+x3int = abs(int(input("Introduce tu un numero entero positivo: ")))
 i = 0
 
 while i <= x3int:
     if i % 2 != 0:
-        print(i)
+        print(f"{i} ")
     i = i + 1
