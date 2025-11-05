8d4e5cb67fa3579420f2d5650cbb753d954ea21c
Amador Hinojosa Gálvez (IZV) <you@example.com>
2025-11-05 10:01:17 +0100

    ADD ejercicio12bucl.py

---
+++ bucles/ejercicio12bucl.py
@@ -0,0 +1,6 @@
+x12frase = input("Introduce una frase: ")
+x12letra = input("Introduce una letra: ")
+
+x12contador = x12frase.count(x12letra)
+
+print(f"La letra '{x12letra}' aparece {x12contador} veces en la frase.")
