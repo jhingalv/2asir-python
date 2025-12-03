53cef0a65d7207325f00c67808cd848aeaea2778
Amador Hinojosa Gálvez (IZV) <you@example.com>
2025-12-03 09:46:51 +0100

    UPDATE ejercicio6litu.py

--- listas-tuplas/ejercicio6litu.py
+++ listas-tuplas/ejercicio6litu.py
@@ -1,14 +1,14 @@
 x6asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
 x6repetir = []
 
-for asignatura in x6asignaturas:
-    nota = float(input(f"¿Qué nota has sacado en {asignatura}? "))
+for x6asignatura in x6asignaturas:
+    nota = float(input(f"¿Qué nota has sacado en {x6asignatura}? "))
     if nota < 5:
-        x6repetir.append(asignatura)
+        x6repetir.append(x6asignatura)
 
 if x6repetir:
     print("\nTienes que repetir las siguientes asignaturas:")
-    for asignatura in x6repetir:
-        print(asignatura)
+    for x6asignatura in x6repetir:
+        print(x6asignatura)
 else:
     print("\n¡Has aprobado todo!")
